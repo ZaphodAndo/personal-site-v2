18efa8c301aba5c1006c9cc6e726e5f6a9d6d14b
Ethan Anderson <ethan.anderson00@gmail.com>
2019-08-11 16:55:36 +0100

    Improved layout of Header.js

--- components/Header.js
+++ components/Header.js
@@ -17,8 +17,12 @@ Router.onRouteChangeError = () => {
 };
 
 const StyledHeader = styled.header`
-    /* position: fixed; */
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
     min-width: 100%;
+    background-color: #202020;
+    box-shadow: 1px 2px 18px rgba(0,0,0,.1);
 
     hr {
         background-color: #5183f5;
@@ -27,15 +31,26 @@ const StyledHeader = styled.header`
         height: 2px;
     }
 
-    .nav-bar {
+    .nav-bar-wrapper {
         display: flex;
         justify-content: center;
+    }
+
+    .nav-bar {
+        display: flex;
         font-size: 1.2rem;
-        background-color: #202020;
-        box-shadow: 1px 2px 18px rgba(0,0,0,.1);
+        max-width: 750px;
+        min-width: 750px;
+
+        @media screen and (max-width: 850px) {
+            min-width: 0;
+        }
     }
 
     .links {
+        align-self: center;
+        margin-left: auto;
+        margin-right: 0;
         padding-top: 19px;
         padding-bottom: 16px;
     }
@@ -69,6 +84,8 @@ const StyledHeader = styled.header`
     .brand {
         display: flex;
         align-items: center;
+        padding-left: 0;
+        margin-left: 0;
     }
     .brand img {
         margin-right: 1rem;
@@ -85,27 +102,30 @@ export default class Header extends Component {
         return (
             <StyledHeader>
                 <hr/>
-                <div className="nav-bar">
-                    <div>
-                        <Link href="/">
-                            <a className="brand">
-                                <img src="../static/favicon-32x32.png" alt="Website logo"/> 
-                                <span>Ethan Anderson</span>
-                            </a>
-                        </Link>
-                    </div>
-                    <div className="links">
-                        <Link href="/">
-                            <a>Home</a>
-                        </Link>
-                        <Link href="/about">
-                            <a>About</a>
-                        </Link>
-                        <Link href="/articles">
-                            <a>Articles</a>
-                        </Link>
+                <div className="nav-bar-wrapper">
+                    <div className="nav-bar">
+                        <div>
+                            <Link href="/">
+                                <a className="brand">
+                                    <img src="../static/favicon-32x32.png" alt="Website logo"/> 
+                                    <span>Ethan Anderson</span>
+                                </a>
+                            </Link>
+                        </div>
+                        <div className="links">
+                            <Link href="/">
+                                <a>Home</a>
+                            </Link>
+                            <Link href="/about">
+                                <a>About</a>
+                            </Link>
+                            <Link href="/articles">
+                                <a>Articles</a>
+                            </Link>
+                        </div>
                     </div>
                 </div>
+                
             </StyledHeader>
         )
     }
